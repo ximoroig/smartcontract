1f536a9f76e50384aea9aee70135227271aee273
ximoroig <99285580+ximoroig@users.noreply.github.com>
2022-02-08 20:27:02 +0000

    Update contract.py

--- contract.py
+++ contract.py
@@ -7,3 +7,10 @@ from web3.providers.websocket import WebsocketProvider
 from web3 import Web3
 from solc import compile_standard
 print("Hello world, import succesful")
+with open("contract.sol") as c:
+ contractText=c.read()
+with open(".pk") as pkfile:
+ privateKey=pkfile.read()
+with open(".infura") as infurafile:
+ infuraKey=infurafile.read()
+print("files read succesfully")
