eb10bfd5791c92c999544bed175f73bb3b0e33c3
ximoroig <99285580+ximoroig@users.noreply.github.com>
2022-02-08 20:51:40 +0000

    Update contract.py

--- contract.py
+++ contract.py
@@ -52,3 +52,26 @@ tx_dict = Greeter.constructor().buildTransaction({
   'nonce': nonce,
   'from':address1
 })
+signed_txn = W3.eth.account.sign_transaction(tx_dict, private_key=privateKey)
+#diagnostics
+#print(signed_txn)
+print("Deploying the Smart Contract")
+result = W3.eth.sendRawTransaction(signed_txn.rawTransaction)
+#diagnostics
+#print(result)
+#print('-----------------------------------')
+tx_receipt = None#W3.eth.getTransactionReceipt(result)
+
+count = 0
+while tx_receipt is None and (count < 30):
+  time.sleep(10)
+  try:
+    tx_receipt = W3.eth.getTransactionReceipt(result)
+  except:
+    print('.')
+
+if tx_receipt is None:
+  print (" {'status': 'failed', 'error': 'timeout'} ")
+#diagnostics
+
+print("Contract address is:",tx_receipt.contractAddress)
