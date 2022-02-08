756785185790996348e64eecc19f85d9ba802327
ximoroig <99285580+ximoroig@users.noreply.github.com>
2022-02-08 20:36:46 +0000

    Update contract.py

--- contract.py
+++ contract.py
@@ -14,3 +14,27 @@ with open(".pk") as pkfile:
 with open(".infura") as infurafile:
  infuraKey=infurafile.read()
 print("files read succesfully")
+compiled_sol = compile_standard({
+    "language": "Solidity",
+    "sources": {
+        "Greeter.sol": {
+            "content": contractText
+        }
+    },
+    "settings":
+        {
+            "outputSelection": {
+                "*": {
+                    "*": [
+                        "metadata", "evm.bytecode"
+                        , "evm.bytecode.sourceMap"
+                    ]
+                }
+            }
+        }
+})
+bytecode = compiled_sol['contracts']['Greeter.sol']['Greeter']['evm']['bytecode']['object']
+abi = json.loads(compiled_sol['contracts']['Greeter.sol']['Greeter']['metadata'])['output']['abi']
+W3 = Web3(WebsocketProvider('wss://ropsten.infura.io/ws/v3/%s'%infuraKey))
+account1=Account.from_key(privateKey);
+print("Account created")
