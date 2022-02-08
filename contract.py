b5258f64e01488334d7973efa34d7256ab54b646
ximoroig <99285580+ximoroig@users.noreply.github.com>
2022-02-08 20:24:11 +0000

    Update contract.py

--- contract.py
+++ contract.py
@@ -1,1 +1,9 @@
-print("Hello world")
+import time
+import json
+import web3
+from eth_account import Account
+from web3.auto import w3
+from web3.providers.websocket import WebsocketProvider
+from web3 import Web3
+from solc import compile_standard
+print("Hello world, import succesful")
